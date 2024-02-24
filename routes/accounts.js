23b2e674a03ec3d79d9e3967d26107184a0968ff
milin0102 <30398940+milin0102@users.noreply.github.com>
2024-02-24 22:40:26 +0530

    Update accounts.js

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,1 +1,86 @@
-routes
+const express = require("express");
+const Account = require("../dal/accounts")
+const mongoose = require("mongoose")
+const User = require("../dal/user");
+const { authenticateToken } = require("../middleware");
+const router = express.Router();
+
+router.get( "/", (req, res) => {
+    res.status(200).send( "Welcome to the Accounts API")
+})
+
+router.post("/balance",async (req,res,next)=>{
+    console.log("request body: " + req.body.userId)
+    try {
+        if(req.body.userId){
+            let userId= req.body.userId;
+            let whereObj = {
+                userId: req.body.userId
+            }
+            await Account.findOne(whereObj).then((response)=>{
+                if(!response){
+                    console.log("what a resp :" + response)
+                    return res.status(400).json({"message":"Account not found"});
+                }else{
+                    response.balance = response.balance/100
+                    return res.status(200).json(response);
+                }
+            }).catch((e)=>{
+                console.log("Error: "+e)
+                throw e;
+            })
+        }else{
+            res.status(411).json({"message":"userId is missing"})
+        }
+    } catch (error) {
+        console.log("Error while fetching balance");
+        throw error;
+    }
+})
+
+router.post("/transfer",async (req,res)=>{
+    try {
+        const session = await mongoose.startSession();
+        session.startTransaction();
+        const receiverId = req.body.to;
+        const amount = req.body.amount;
+        if(receiverId && amount){
+            let receiverUser = await Account.findOne({userId:receiverId}).session(session).catch((e)=>{
+                console.log('Error while finding receiver id: '+e);
+                throw e; 
+            });
+            console.log(receiverUser);
+            if(!receiverUser){
+                await session.abortTransaction();
+                res.status(400).json({"message":"Invalid Account"})
+
+            }else{
+                let fromAccount = await Account.findOne({userId:req.body.from}).session(session).catch((e)=>{
+                    console.log('Error while finding receiver id: '+e);
+                    throw e; 
+                });
+                if((fromAccount.balance/100) < amount){
+                    await session.abortTransaction();
+                    res.status(400).json({"status":false,"message":"Insufficient Balance"})
+                }else{
+                    await Account.updateOne({userId:req.body.from},{"$inc":{balance:-(amount*100)}});
+                    await Account.updateOne({userId:req.body.to},{"$inc":{balance:(amount*100)}});
+
+                    await session.commitTransaction();
+                    res.status(200).json({"status":true , "message":"Transaction Successful!"});
+                }
+            }
+        
+
+        }else{
+            res.status(500).json({"message":"Insufficient paramter"})
+        }
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error)
+    }
+
+})
+
+module.exports = router
