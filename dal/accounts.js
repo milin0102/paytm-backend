284874df0e718546f8b49efe375a9fa602f6c333
milin0102 <30398940+milin0102@users.noreply.github.com>
2024-02-24 22:43:49 +0530

    Update accounts.js

--- dal/accounts.js
+++ dal/accounts.js
@@ -1,1 +1,16 @@
+const mongoose = require("mongoose");
 
+const accountSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    balance:{
+        type: Number,
+        required:true
+    }
+})
+const Account = mongoose.model('Account',accountSchema)
+
+module.exports = Account
