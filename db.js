4a2f2cce708f4ee8d572c80b96ceeec1672c6efa
milin0102 <milinshah0102@gmail.com>
2024-02-27 22:34:29 +0530

    Mongo db conncection string changes

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect('mongodb+srv://milin0102:milin0102@cluster0.fzgaw.mongodb.net/paytm').then(()=>{
+mongoose.connect('mongodb+srv://milin0102:milin%40123@cluster0.fzgaw.mongodb.net/paytm').then(()=>{
     console.log("connection established")
 }).catch((e)=>{
     console.log("Connection not Established "+e);
